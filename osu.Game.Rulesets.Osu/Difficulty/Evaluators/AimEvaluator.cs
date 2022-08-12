a1e0ca6a02d0daccc7ba9d5b9ad5f686b29e083f
tsunyoku <mbruhyo@gmaiil.com>
2022-08-12 15:34:36 +0100

    Buff tech

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -14,7 +14,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
     {
         private const double wide_angle_multiplier = 1.5;
         private const double acute_angle_multiplier = 1.95;
-        private const double slider_multiplier = 1.35;
+        private const double slider_multiplier = 1.5;
         private const double velocity_change_multiplier = 0.75;
 
         /// <summary>
